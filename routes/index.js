3e4566f63abeb9a2653a822329f7ca764998497f
jepra777 <jere.prasetyo.jp@gmail.com>
2022-06-17 20:00:09 +0700

    Add Endpoint Admin Data

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,7 @@ const authentication = require('../middleware/authentication')
 router.get("/",(req,res)=> res.send("API Showcarpedia Version 1.0"))
 router.get("/v1/get-collection",(req,res)=> res.send("Showcarpedia Collection List 2022"))
 router.get("/v1/get-user",(req,res)=> res.send("Showcarpedia User List 2022"))
+router.get("/v1/get-admin-data",(req,res)=> res.send("Showcarpedia Admin List 2022"))
 router.use("/v1", v1)
 
 //Testing Auth
