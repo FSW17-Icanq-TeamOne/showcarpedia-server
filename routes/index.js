618c65a49b43004b82884753034825f74d601e01
jepra777 <jere.prasetyo.jp@gmail.com>
2022-06-17 18:44:23 +0700

    Feat: CI/CD

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,8 @@ const router = require("express").Router()
 const v1 = require("./v1")
 const authentication = require('../middleware/authentication')
 
-router.get("/",(req,res)=> res.send("udah masuk"))
-
+router.get("/",(req,res)=> res.send("API Showcarpedia Version 1.0"))
+router.get("/v1/get-collection",(req,res)=> res.send("Showcarpedia Collection List 2022"))
 router.use("/v1", v1)
 
 //Testing Auth
